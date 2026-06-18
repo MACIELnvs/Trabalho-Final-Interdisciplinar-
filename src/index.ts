011978349f19c3462f461678a8fa127590c6b94c
Heloisa <poleseheloisa24@gmail.com>
2026-06-18 11:25:48 -0300

    take api data

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,14 @@
 import { Monstro } from "./Monstro";
 import { Feitico } from "./Feitico";
 import { Armadilha } from "./Armadilha";
+import { fetchYuGiOh } from "./services/takeData";
+
 
 const feitico1 = new Feitico(2,"pinrimplimplim", "...", "peidim", [] , "fogo");
 const monstro1 = new Monstro(1,"chubaka","...","cabeludinho",[],233,2,3,"monstro");
 const Armadilha1 = new Armadilha(3,"corda","...", "forte", [],"");
 //console.log(feitico1);
+
+
+fetchYuGiOh();
+
