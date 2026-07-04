3ff833d8a5302a89d3a38530035a27ad13f2f416
ceciliadumer <ceciliadumer@gmail.com>
2026-07-04 15:23:51 -0300

    depuração

--- src/index.ts
+++ src/index.ts
@@ -17,10 +17,38 @@ app.use("/cartas", cartaRoutes);
 
 const PORT = process.env.PORT || 3000;
 
+async function fetchECriar(): Promise<void> {
+  try {
+    console.log("Banco vazio. Buscando cartas na API...");
+
+    const response = await fetch(
+      "https://db.ygoprodeck.com/api/v7/cardinfo.php"
+    );
+
+    if (!response.ok) {
+      throw new Error(`Erro na API. Status: ${response.status}`);
+    }
+
+    const responseJson = await response.json();
+
+    await controller.criarCartas(responseJson);
+
+    console.log("Cartas buscadas na API e salvas no banco.");
+  } catch (error: any) {
+    console.error("Erro ao buscar dados da API:", error.message);
+  }
+}
+
 async function iniciar() {
   try {
     await testConnection();
+
     await controller.carregarDoBanco();
+
+    if (controller.listar().length === 0) {
+      await fetchECriar();
+      await controller.carregarDoBanco();
+    }
 
     app.listen(PORT, () => {
       console.log(`Servidor rodando em http://localhost:${PORT}`);
